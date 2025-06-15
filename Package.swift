4e83d096852f61b96d753e3a5e0ea184ea740d56
Tespera <your-email@example.com>
2025-06-15 15:06:36 +0800

    优化Package.swift配置

--- Package.swift
+++ Package.swift
@@ -24,14 +24,7 @@ let package = Package(
                 ".DS_Store",
                 ".cursor/",
                 ".vscode/",
-<<<<<<< HEAD
-                "*.dmg",
-                "*.md",
-                "*.sh",
-                "*.md"
-=======
                 "*.dmg"
->>>>>>> dev
             ]
         )
     ]
